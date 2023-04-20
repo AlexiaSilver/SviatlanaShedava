cdf45ce1acbc6fb13ab13f922b884cecd0f90315
AlexiaSilver <shedova.s.a@gmail.com>
2023-04-20 08:53:45 +0200

    [007] Add tasks for homework 7 (collections)

---
+++ src/com/epam/javacourse/homework7/StringOperations.java
@@ -0,0 +1,33 @@
+package com.epam.javacourse.homework7;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class StringOperations {
+    public static List<String> sortLinesByLength(String filePath) throws IOException {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        }
+        Collections.sort(lines, Comparator.comparingInt(String::length));
+        return lines;
+    }
+    public static boolean checkParentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
